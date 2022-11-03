0e8025bca01d6a2745ab59c8e8df626e44d27540
Justin Ellenbecker <strifejester@gmail.com>
2022-11-03 12:59:32 -0500

    Final Changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'This is an express page' });
+  res.render(
+    'index', 
+    { title: 'This is an express page' countDown: 'Making a countdown'});
 });
 
 module.exports = router;
